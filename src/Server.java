37ee6d0d27cf0100171c8554c352b35a3ea19093
hamza <hamza.benkhoud@etudiant-isi.utm.tn>
2021-12-09 18:46:00 +0100

    done

--- src/Server.java
+++ src/Server.java
@@ -2,6 +2,7 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Locale;
 
 public class Server extends UnicastRemoteObject implements MyInterface {
 
@@ -14,4 +15,38 @@ public class Server extends UnicastRemoteObject implements MyInterface {
 
         Naming.rebind("rmi://127.0.0.1:1250/server", server);
     }
+
+    @Override
+    public String  reverse(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+    @Override
+    public char minChar(String s){
+        char c =s.charAt(0);
+        for (int i = 1; i <s.length()  ; i++) {
+            if(s.charAt(i) < c){
+                c=s.charAt(i);
+            }
+        }
+        return c;
+    }
+    @Override
+    public String upperLower(String s){
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < s.length() ; i++) {
+            if(s.charAt(i) >='a' && s.charAt(i) <='z'){
+                res.append((char)(s.charAt(i) - 32));
+                /*res.append(Character.toString(s.charAt(i)).toUpperCase(Locale.ROOT));*/
+
+            }else if(s.charAt(i) >='A' && s.charAt(i) <='Z'){
+                res.append((char)(s.charAt(i) + 32));
+                /*res.append(Character.toString(s.charAt(i)).toLowerCase(Locale.ROOT));*/
+            }else{
+               res.append(s.charAt(i));
+            }
+        }
+
+        return res.toString();
+    }
+
 }
